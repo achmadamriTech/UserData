34bdd4143c2b587954cf41cee50ea611056718e2
unknown <nyamukterbang8@gmail.com>
2021-08-27 08:34:11 +0700

    Get dataById Role

--- src/main/java/com/example/userdata/service/RoleService.java
+++ src/main/java/com/example/userdata/service/RoleService.java
@@ -1,12 +1,15 @@
 package com.example.userdata.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.userdata.entity.Role;
 import com.example.userdata.repository.RoleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class RoleService {
@@ -17,5 +20,16 @@ public class RoleService {
     public List<Role> getAllRole(){
         return roleRepository.findAll();
     }
+
+    // Get Data Berdasarkan ID
+    public Role getRoleById(Long roleId){
+        Optional<Role> role = roleRepository.findById(roleId);
+
+        if (role.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Role tidak Ditemukan!");
+        }
+        return role.get();
+    }
+
 
 }
