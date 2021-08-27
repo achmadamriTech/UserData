31d6cf1ba579b08aaf9f8e287fc84227155ab470
unknown <nyamukterbang8@gmail.com>
2021-08-27 08:23:24 +0700

    Create GetAllData RoleService

--- src/main/java/com/example/userdata/service/RoleService.java
+++ src/main/java/com/example/userdata/service/RoleService.java
@@ -1,8 +1,21 @@
 package com.example.userdata.service;
 
+import java.util.List;
+
+import com.example.userdata.entity.Role;
+import com.example.userdata.repository.RoleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService {
-    
+    @Autowired
+    private RoleRepository roleRepository;
+
+    // Get Semua Data
+    public List<Role> getAllRole(){
+        return roleRepository.findAll();
+    }
+
 }
