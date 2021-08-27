af80b5c869372d5306808b64a69e0ba6a52c318c
unknown <nyamukterbang8@gmail.com>
2021-08-27 07:44:31 +0700

    Create GetUsersById

--- src/main/java/com/example/userdata/repository/UsersRepository.java
+++ src/main/java/com/example/userdata/repository/UsersRepository.java
@@ -1,5 +1,9 @@
 package com.example.userdata.repository;
 
+
+
+import java.util.Optional;
+
 import com.example.userdata.entity.Users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
-    
+
+    Optional<Users> findByID(Long usersId);
 }
--- src/main/java/com/example/userdata/service/UsersService.java
+++ src/main/java/com/example/userdata/service/UsersService.java
@@ -1,12 +1,15 @@
 package com.example.userdata.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.userdata.entity.Users;
 import com.example.userdata.repository.UsersRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UsersService {
@@ -16,5 +19,15 @@ public class UsersService {
         // Get Semua Data
     public List<Users> getAllUsers(){
         return usersRepository.findAll();
+    }
+
+    // Get Data Berdasarkan ID
+    public Users getUsersById(Long usersId){
+        Optional<Users> users = usersRepository.findById(usersId);
+
+        if (users.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Users tidak Ditemukan!");
+        }
+        return users.get();
     }
 }
