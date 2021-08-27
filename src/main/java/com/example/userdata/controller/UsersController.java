3fb2748f5a07a4f0f2612ce428b662e20cd74f24
unknown <nyamukterbang8@gmail.com>
2021-08-27 07:28:36 +0700

    Create Controller  UserController

---
+++ src/main/java/com/example/userdata/controller/UsersController.java
@@ -0,0 +1,28 @@
+package com.example.userdata.controller;
+
+import java.util.List;
+
+import com.example.userdata.entity.Users;
+import com.example.userdata.service.UsersService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+    
+    private UsersService usersService;
+
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    // Get Semua Data
+    @GetMapping("")
+    public List<Users> getAllUsers(){
+        return usersService.getAllUsers();
+    }
+}
